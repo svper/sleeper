1de0e894c475ec14e159ed87804befb879d801d5
Robin Verhulst <svper@users.noreply.github.com>
2022-09-14 15:14:55 +0200

    small fixes

--- kid-sleeper/src/app/app.component.ts
+++ kid-sleeper/src/app/app.component.ts
@@ -9,8 +9,15 @@ import NoSleep from 'nosleep.js';
 })
 export class AppComponent {
   title = 'kid-sleeper';
-  possibleImages = ['https://i.pinimg.com/736x/6d/d2/0d/6dd20dc6562db8781a116d89926b41fc.jpg']
-  selectedBackgroundImage = this.possibleImages[0];
+  possibleImages = [
+    'https://i.pinimg.com/736x/6d/d2/0d/6dd20dc6562db8781a116d89926b41fc.jpg',
+    'https://d3ddkgxe55ca6c.cloudfront.net/assets/t1625198549/a/36/9b/paw-patrol-snuggle-prv-3-2020426.jpg',
+    'https://d3ddkgxe55ca6c.cloudfront.net/assets/t1625198541/a/6b/7b/paw-patrol-snuggle-prv-2-2020422.jpg',
+    'https://c.tenor.com/ic3qongpg3QAAAAd/paw-patrol-tired.gif',
+    'https://static.wikia.nocookie.net/paw-patrol/images/6/64/Chickaletta%27s_Egg_12.jpg/revision/latest/scale-to-width-down/1200?cb=20190716131441'
+  ];
+  selectedBackgroundImageNumber = 3;
+  selectedBackgroundImage = this.possibleImages[this.selectedBackgroundImageNumber];
   totalMinutes = 20;
   totalSeconds = this.totalMinutes * 60;
   remainingSeconds = this.totalMinutes * 60;
@@ -24,6 +31,8 @@ export class AppComponent {
 
   noSleep: NoSleep = new NoSleep();
 
+  audioFinish = new Audio('../assets/Pup_Bedtime.mp3');
+
   constructor() {
 
   }
@@ -35,6 +44,10 @@ export class AppComponent {
     this.timerSub = source.subscribe(val => this.tick(val));
     this.timerIsRunning = true;
     this.noSleep.enable();
+    this.audioFinish.load();
+    this.audioFinish.play();
+    this.audioFinish.currentTime = 0
+    this.audioFinish.pause();
   }
 
   continueTimer = () => {
@@ -79,9 +92,7 @@ export class AppComponent {
   }
 
   playAudio() {
-    let audio = new Audio('../assets/Pup_Bedtime.mp3');;
-    audio.load();
-    audio.play();
+    this.audioFinish.play();
   }
 
 }
